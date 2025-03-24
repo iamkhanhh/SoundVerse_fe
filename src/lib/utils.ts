d4177e7ce4678c288c0657716b8534a20ef44369
Nguyễn Quốc Khánh <khanh9102004@gmail.com>
2025-03-24 23:22:36 +0700

    update

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,5 +5,5 @@ export function cn(...inputs:ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 export const apiUrl = {
-  baseURL: "https://soundverse-be.onrender.com/api"
+  baseURL: "http://54.238.187.71:8080/api"
 };
